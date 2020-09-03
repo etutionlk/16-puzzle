5bb94fc4f9ea837f28cb2f57980b696e662062e4
cng.chinthakam <cng.chinthakam@gmail.com>
2020-09-03 11:51:41 +0530

    complete board

--- src/Board.js
+++ src/Board.js
@@ -4,39 +4,38 @@ import Square from "./Square";
 
 class Board extends Component {
     renderSquare(i) {
-        return <Square />;
+        const {board} = this.props.state
+
+        return <Square value={board[i]===0 ? "":board[i]}/>;
     }
 
     render() {
-        const style ={
-            verticalAlign:'middle'
-        }
         return (
-            <div style={style}>
+            <div>
                 <div className="status">{status}</div>
-                <div className="row">
+                <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
                     {this.renderSquare(2)}
                     {this.renderSquare(3)}
                 </div>
-                <div className="row">
+                <div className="board-row">
                     {this.renderSquare(4)}
                     {this.renderSquare(5)}
                     {this.renderSquare(6)}
                     {this.renderSquare(7)}
                 </div>
-                <div className="row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
+                <div className="board-row">
                     {this.renderSquare(8)}
-                    {this.renderSquare(2)}
+                    {this.renderSquare(9)}
+                    {this.renderSquare(10)}
+                    {this.renderSquare(11)}
                 </div>
-                <div className="row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                    {this.renderSquare(2)}
+                <div className="board-row">
+                    {this.renderSquare(12)}
+                    {this.renderSquare(13)}
+                    {this.renderSquare(14)}
+                    {this.renderSquare(15)}
                 </div>
             </div>
         );
--- src/index.js
+++ src/index.js
@@ -8,12 +8,22 @@ class Game extends Component {
     constructor(props) {
         super(props);
 
+        let board = new Array(16);
+        for (let i = 0; i < board.length; ++i) board[i] = i;
+
         this.state = {
             selectValue: 4,
             moves:0,
+            board:this.shuffle(board),
             progress:"50%"
         };
         this.selectLevel = this.selectLevel.bind(this);
+    }
+
+    shuffle(o) {
+        const temp = o.slice();
+        for(var j, x, i = temp.length; i; j = Math.floor(Math.random() * i), x = temp[--i], temp[i] = temp[j], temp[j] = x);
+        return temp;
     }
 
     selectLevel(e) {
@@ -30,38 +40,38 @@ class Game extends Component {
         }
         return (
             <div className="container">
-                <div className="row" style={styles}>
-                    <div className="col-lg-4 col-md-4 col-sm-4">
-                        <label className="label">Select Level</label>
-                        <select name="level" className="custom-select" onChange={this.selectLevel}>
-                            <option value="4">4 x 4 board</option>
-                            <option value="5">5 x 5 board</option>
-                            <option value="6">6 x 6 board</option>
-                        </select>
-                    </div>
-                    <div className="col-lg-4 col-md-4 col-sm-4">
-                        <label className="label">Progress</label>
-                        <br/>
-                        <div className="progress">
-                            <div className="progress-bar" role="progressbar" style={{width:this.state.progress}}
-                                 aria-valuetext="25"
-                                 aria-valuemin="0" aria-valuemax="100">{this.state.progress}
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-4 col-md-4 col-sm-4">
-                        <label className="label">Moves:</label>
-                        <br/>
-                        <label className="badge badge-warning">{this.state.moves}</label>
-                    </div>
-                </div>
+                {/*<div className="row" style={styles}>*/}
+                {/*    <div className="col-lg-4 col-md-4 col-sm-4">*/}
+                {/*        <label className="label">Select Level</label>*/}
+                {/*        <select name="level" className="custom-select" onChange={this.selectLevel}>*/}
+                {/*            <option value="4">4 x 4 board</option>*/}
+                {/*            <option value="5">5 x 5 board</option>*/}
+                {/*            <option value="6">6 x 6 board</option>*/}
+                {/*        </select>*/}
+                {/*    </div>*/}
+                {/*    <div className="col-lg-4 col-md-4 col-sm-4">*/}
+                {/*        <label className="label">Progress</label>*/}
+                {/*        <br/>*/}
+                {/*        <div className="progress">*/}
+                {/*            <div className="progress-bar" role="progressbar" style={{width:this.state.progress}}*/}
+                {/*                 aria-valuetext="25"*/}
+                {/*                 aria-valuemin="0" aria-valuemax="100">{this.state.progress}*/}
+                {/*            </div>*/}
+                {/*        </div>*/}
+                {/*    </div>*/}
+                {/*    <div className="col-lg-4 col-md-4 col-sm-4">*/}
+                {/*        <label className="label">Moves:</label>*/}
+                {/*        <br/>*/}
+                {/*        <label className="badge badge-warning">{this.state.moves}</label>*/}
+                {/*    </div>*/}
+                {/*</div>*/}
 
                 <div className="row" style={styles}>
-                    <div className="col-lg-4 col-md-4 col-sm-4"></div>
-                    <div className="col-lg-4 col-md-4 col-sm-4">
-                        <Board />
+                    <div className="col-lg-3 col-md-3 col-sm-3"></div>
+                    <div className="col-lg-6 col-md-6 col-sm-6">
+                        <Board state={this.state}/>
                     </div>
-                    <div className="col-lg-4 col-md-4 col-sm-4"></div>
+                    <div className="col-lg-3 col-md-3 col-sm-3"></div>
 
                 </div>
 
